d1dff119ea12328a00309fb3b846dec0c3f11edb
denizetkar <denizetkar@gmail.com>
2019-11-07 04:26:52 +0300

    added bnf parser with Lark, added BNF of ANSI-C11 standard

---
+++ tests/bnf_lark_test.py
@@ -0,0 +1,51 @@
+from collections import namedtuple
+import os
+
+from lark import Lark
+
+import utils
+
+NonTerminal = namedtuple('NonTerminal', ['name'])
+Terminal = namedtuple('Terminal', ['name'])
+
+grammar_file_path = os.path.join('..', 'data', 'bnf.lark')
+with open(grammar_file_path) as f:
+    parser = Lark(f)
+
+text_file_path = os.path.join('..', 'data', 'C_lang.bnf')
+with open(text_file_path) as f:
+    tree = parser.parse(f.read())
+
+print(utils.pretty(tree))
+
+rules_dict = {}
+syntax = tree.children[0]
+for syntax_child in syntax.children:
+    if syntax_child.data == 'comment':
+        continue
+    rule = syntax_child
+    non_terminal, expression = rule.children[0], rule.children[4]
+    rule_text = non_terminal.children[1]
+    rule_name_obj = NonTerminal(''.join([token.value for token in rule_text.children]))
+
+    expression_obj = []
+    for sequence_index in range(0, len(expression.children), 4):
+        sequence = expression.children[sequence_index]
+        sequence_obj = []
+        for term_index in range(0, len(sequence.children), 2):
+            term = sequence.children[term_index]
+            if term.children[0].data == 'terminal':
+                terminal = term.children[0]
+                text_i = terminal.children[1]
+                term_obj = Terminal(utils.decode_escapes(''.join([token.value for token in text_i.children])))
+            else:  # term.children[0].data == 'non_terminal'
+                rhs_non_terminal = term.children[0]
+                rhs_rule_text = rhs_non_terminal.children[1]
+                term_obj = NonTerminal(''.join([token.value for token in rhs_rule_text.children]))
+            sequence_obj.append(term_obj)
+        expression_obj.append(sequence_obj)
+
+    rules_dict[rule_name_obj] = expression_obj
+
+print('---------------------------------')
+utils.print_rules_dict(rules_dict)
---
+++ utils.py
@@ -0,0 +1,89 @@
+import codecs
+import io
+import re
+import sys
+
+import lark
+
+
+def pretty(tree, stream=None):
+    if stream is None:
+        stream = io.StringIO()
+    pretty_stream(tree, stream, 0)
+    return stream.getvalue()
+
+
+def pretty_stream(tree, stream, depth=0):
+    indentation = ' ' * depth * 2
+    if isinstance(tree, lark.Tree):
+        print(indentation + tree.data, file=stream)
+        for t in tree.children:
+            pretty_stream(t, stream, depth + 1)
+    else:
+        print(indentation + '%s %s' % (tree.type, repr(tree.value)), file=stream)
+
+
+def _recurse_tree(tree, func):
+    return list(map(func, tree.children)) if isinstance(tree, lark.Tree) else tree
+
+
+def flatten_unnecessary_nodes(tree):
+    new = _recurse_tree(tree, flatten_unnecessary_nodes)
+    if isinstance(tree, lark.Tree):
+        tree.children = new
+        if len(new) == 1 and isinstance(new[0], lark.Tree):
+            tree = new[0]
+    return tree
+
+
+def print_rules_dict(rules_dict, stream=sys.stdout):
+    indentation = ' ' * 4
+    for rule, expression in rules_dict.items():
+        print(str(rule) + ':', file=stream)
+        for sequence in expression:
+            print(indentation + str(sequence), file=stream)
+
+
+ESCAPE_SEQUENCE_RE = re.compile(r'''
+    ( \\U........      # 8-digit hex escapes
+    | \\u....          # 4-digit hex escapes
+    | \\x..            # 2-digit hex escapes
+    | \\[0-7]{1,3}     # Octal escapes
+    | \\N\{[^}]+\}     # Unicode characters by name
+    | \\[\\'"abfnrtv]  # Single-character escapes
+    )''', re.UNICODE | re.VERBOSE)
+
+
+def decode_escapes(s):
+    def decode_match(match):
+        return codecs.decode(match.group(0), 'unicode-escape')
+
+    return ESCAPE_SEQUENCE_RE.sub(decode_match, s)
+
+
+class Vocabulary:
+    def __init__(self, oov_token):
+        self._unused_value = 0
+        self._token_to_value = {}
+        self._value_to_token = {}
+        self.oov_token = oov_token
+        self.add_token(oov_token)
+
+    def add_token(self, token):
+        if token not in self._token_to_value:
+            self._token_to_value[token] = self._unused_value
+            self._value_to_token[self._unused_value] = token
+            self._unused_value += 1
+
+    def value_of_token(self, token):
+        if token not in self._token_to_value:
+            return self._token_to_value[self.oov_token]
+        return self._token_to_value[token]
+
+    def token_of_value(self, value):
+        if value not in self._value_to_token:
+            return self.oov_token
+        return self._value_to_token[value]
+
+    def vocab_size(self):
+        return len(self._token_to_value)
